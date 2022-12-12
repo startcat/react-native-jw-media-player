f10f8a75d0518c11c76299cdcf13a3331352e82b
Daniel Marín <daniel.marin@start.cat>
2022-12-12 13:20:24 +0100

    Youbora

---
+++ ios/RNJWPlayer/platform/YouboraJWPlayerAdapter.framework/Headers/YBJWPlayerAdsAdapter.h
@@ -0,0 +1,14 @@
+//
+//  YBJWAdsAdapter.h
+//  YouboraJWPlayerAdapter
+//
+//  Created by Enrique Alfonso Burillo on 20/10/2017.
+//  Copyright © 2017 NPAW. All rights reserved.
+//
+
+#import <YouboraLib/YouboraLib.h>
+#import <JWPlayer_iOS_SDK/JWPlayerController.h>
+
+@interface YBJWPlayerAdsAdapter : YBPlayerAdapter<JWPlayerController*>
+
+@end
